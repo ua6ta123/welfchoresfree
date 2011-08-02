869c13ad70aafb875c23cce9774a338b75de606c
Fumiaki Sakamoto <ua6ta123@bma.biglobe.ne.jp>
2011-08-02 14:13:07 +0900

    first implement

---
+++ welf/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Welf::Application.config.secret_token = '94e8b7ec50e2ab9961a8c1bc090d538a9a14e1c3909ac5c47f595b38e5c7bda4b3ebaa72a8bbd0ffa38988ec91d027f7fd58aa8eff2191a71c94b5b13c96de6f'
